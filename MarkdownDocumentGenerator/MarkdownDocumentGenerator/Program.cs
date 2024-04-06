18d06281d75dfebaba9a217b4ae3ab4fb01afb09
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-07 00:07:00 +0900

    DTOプロジェクト読み込めるようにした

--- MarkdownDocumentGenerator/MarkdownDocumentGenerator/Program.cs
+++ MarkdownDocumentGenerator/MarkdownDocumentGenerator/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace MarkdownDocumentGenerator
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.Configuration;
+
+namespace MarkdownDocumentGenerator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var dtoProjectFilePath = configuration["DTOProjectPath"];
+
+            MSBuildLocator.RegisterDefaults();
+            using var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(dtoProjectFilePath);
         }
     }
 }
