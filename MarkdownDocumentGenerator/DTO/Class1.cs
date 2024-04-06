349077f95b23424afd9dcd8bf9c2cc663813890e
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-06 23:48:36 +0900

    DTO定義用の別プロジェクトを追加した

---
+++ MarkdownDocumentGenerator/DTO/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace DTO
+{
+    public abstract class DTOBase { }
+
+    /// <summary>
+    /// プレイヤー
+    /// </summary>
+    public class Player : DTOBase
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// 体力
+        /// </summary>
+        public int Hp { get; set; }
+    }
+
+    /// <summary>
+    /// ゲーム情報
+    /// </summary>
+    public class GameInfo : DTOBase
+    {
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        public string Title { get; set; } = "";
+
+        /// <summary>
+        /// 参加者
+        /// </summary>
+        public Player[] Players { get; set; } = [];
+    }
+}
