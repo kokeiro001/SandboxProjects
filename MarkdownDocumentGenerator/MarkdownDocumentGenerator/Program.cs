4e8b94df47fc1a35d1bd6bb7716dffb8586a961f
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-06 23:18:31 +0900

    検証プロジェクトを追加した

---
+++ MarkdownDocumentGenerator/MarkdownDocumentGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MarkdownDocumentGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
