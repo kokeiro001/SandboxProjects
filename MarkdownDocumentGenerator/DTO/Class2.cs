cb75823f6cd358648ca8f65deec4a7312ba1e51a
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-07 14:16:46 +0900

    検証用DTOを追加した

---
+++ MarkdownDocumentGenerator/DTO/Class2.cs
@@ -0,0 +1,41 @@
+﻿namespace DTO
+{
+    /// <summary>
+    /// GODプレイヤー
+    /// </summary>
+    public class GodPlayer : Player
+    {
+        /// <summary>
+        /// GODランク
+        /// </summary>
+        public int Rank { get; set; }
+    }
+
+    /// <summary>
+    /// 循環参照プレイヤー
+    /// </summary>
+    public class CircularReferencePlayer : Player
+    {
+        /// <summary>
+        /// 親
+        /// </summary>
+        public CircularReferencePlayer? Parent { get; set; }
+
+        /// <summary>
+        /// 子供
+        /// </summary>
+        public CircularReferencePlayer[] Children { get; set; } = [];
+    }
+
+
+    /// <summary>
+    /// GODゲーム情報
+    /// </summary>
+    public class GodGameInfo : GameInfo
+    {
+        /// <summary>
+        /// ワールドID
+        /// </summary>
+        public int WorldId { get; set; }
+    }
+}
