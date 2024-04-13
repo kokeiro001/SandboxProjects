0f93876040a36621043382b6058eeaee62dcca50
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-13 12:48:32 +0900

    update

--- SerilogSandbox/SerilogSandbox/Program.cs
+++ SerilogSandbox/SerilogSandbox/Program.cs
@@ -2,63 +2,61 @@
 using Serilog;
 using Serilog.Events;
 
-namespace SerilogSandbox
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var stringValue = "中断したときに通常のブレークポイントと同じように値の中身が見れることを確認する用のローカル変数";
+var intValue = 123;
+var now = DateTime.Now; ;
+
+var logger = MyLoggerFactory.CreateLogger<Program>();
+
+logger.Verbose("{str} {int} {now}", stringValue, intValue, now);
+
+logger.Verbose("Verbose!");
+logger.Debug("Debug!");
+logger.Information("Information!");
+logger.Warning("Warning!このレベルはまだ中断されないよー");
+logger.Error("Error!ブレークポイント張ったときのようにここで中断されるよー");
+logger.Fatal("Fatal!ブレークポイント張ったときのようにここで中断されるよー");
+
+class MyLoggerFactory
 {
-    internal class Program
+    public static ILogger CreateLogger<TSource>()
     {
-        static void Main()
-        {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
+        var baseLogger = Log.ForContext<TSource>();
 
-            var logger = MyLoggerFactory.CreateLogger<Program>();
+        var myLogger = new MyLogger(baseLogger);
 
-            logger.Verbose("Verbose!");
-            logger.Debug("Debug!");
-            logger.Information("Information!");
-            logger.Warning("Warning!");
-            logger.Error("Error!");
-            logger.Fatal("Fatal!");
-        }
+        return myLogger;
     }
+}
 
-    public class MyLoggerFactory
+class MyLogger(ILogger baseLogger) : ILogger
+{
+    /// <summary>
+    /// これ以上のレベルのLogEventが発生したらDebugger.Break()を呼び出す。
+    /// </summary>
+    public LogEventLevel BreakIfGreaterThan { get; set; } = LogEventLevel.Error;
+
+    // この属性を付けると、このメソッド内でデバッガーがステップインしない。
+    // logger.Errorとか呼び出したところでブレークポイント貼られてたみたいな挙動になる。
+    [DebuggerStepThrough]
+    public void Write(LogEvent logEvent)
     {
-        public static ILogger CreateLogger<TSource>()
+        baseLogger.Write(logEvent);
+
+        if (!Debugger.IsAttached)
         {
-            var baseLogger = Log.ForContext<TSource>();
-
-            var myLogger = new MyLogger(baseLogger);
-
-            return myLogger;
+            return;
         }
-    }
 
-    public class MyLogger(ILogger baseLogger) : ILogger
-    {
-        /// <summary>
-        /// これ以上のレベルのLogEventが発生したらDebugger.Break()を呼び出す。
-        /// </summary>
-        public LogEventLevel BreakIfGreaterThan { get; set; } = LogEventLevel.Error;
-
-        // この属性を付けると、このメソッド内でデバッガーがステップインしない。
-        // logger.Errorとか呼び出したところでブレークポイント貼られてたみたいな挙動になる。
-        [DebuggerStepThrough]
-        public void Write(LogEvent logEvent)
+        if (logEvent.Level >= BreakIfGreaterThan)
         {
-            baseLogger.Write(logEvent);
-
-            if (!Debugger.IsAttached)
-            {
-                return;
-            }
-
-            if (logEvent.Level >= BreakIfGreaterThan)
-            {
-                // DebuggerStepThroughがないとここでブレークする。ステップアウトが必要で不便。
-                Debugger.Break();
-            }
+            // DebuggerStepThroughがないとここでブレークする。ステップアウトが必要で不便。
+            Debugger.Break();
         }
     }
 }
