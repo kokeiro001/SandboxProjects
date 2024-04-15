e14dd673e21f7da80fd29d9c87b2d24f811a67d3
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-15 22:31:49 +0900

    出力フォーマット検証

--- SerilogSandbox/OutputFormatSandbox/Program.cs
+++ SerilogSandbox/OutputFormatSandbox/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace OutputFormatSandbox
+﻿using Serilog;
+using Serilog.Exceptions;
+
+namespace OutputFormatSandbox
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LogExceptionSandbox(false, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Exception}{Properties:j}{NewLine}");
+
+            LogExceptionSandbox(true, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Exception}{Properties}{NewLine}");
+
+            LogExceptionSandbox(true, "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{Exception} {Properties:j}{NewLine}");
+        }
+
+        private static void LogExceptionSandbox(bool isEnableEnrichExceptionDetails, string outputTemplate, int dummyArrayIndex = 100)
+        {
+
+            var loggerConfiguration = new LoggerConfiguration()
+                .WriteTo.Console(outputTemplate: outputTemplate);
+
+            if (isEnableEnrichExceptionDetails)
+            {
+                loggerConfiguration = loggerConfiguration
+                    .Enrich.WithExceptionDetails();
+            }
+
+            var logger = loggerConfiguration
+                .CreateLogger();
+
+            var contextLogger = logger.ForContext<Program>();
+
+            contextLogger.Information("isEnableEnrichExceptionDetails: {isEnableEnrichExceptionDetails} outputTemplate: {outputTemplate}",
+                isEnableEnrichExceptionDetails, outputTemplate);
+
+            try
+            {
+                ArgumentOutOfRangeException.ThrowIfNotEqual(dummyArrayIndex, 0);
+            }
+            catch (Exception exception)
+            {
+                contextLogger.Error(exception, "検証エラー");
+            }
+
         }
     }
 }
