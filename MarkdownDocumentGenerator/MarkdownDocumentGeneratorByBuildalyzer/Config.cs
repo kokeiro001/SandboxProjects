31350b1073cb641969fc494e1300cad28f6d2b2b
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-29 17:51:30 +0900

    とりあえず雑コピー

---
+++ MarkdownDocumentGenerator/MarkdownDocumentGeneratorByBuildalyzer/Config.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MarkdownDocumentGeneratorByBuildalyzer
+{
+    public record Config
+    {
+        public string TargetBaseClassName { get; init; } = ""; // ex. DTO.DTOBase
+
+        public string ProjectPath { get; init; } = "";
+
+        public string OutputMarkdownDirectory { get; init; } = "";
+
+        public static void Validate([NotNull] Config? config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+
+            var stringBuilder = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(config.TargetBaseClassName))
+            {
+                stringBuilder.AppendLine($"{nameof(TargetBaseClassName)} is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(config.ProjectPath))
+            {
+                stringBuilder.AppendLine($"{nameof(ProjectPath)} is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(config.OutputMarkdownDirectory))
+            {
+                stringBuilder.AppendLine($"{nameof(OutputMarkdownDirectory)} is required.");
+            }
+
+            if (stringBuilder.Length > 0)
+            {
+                throw new ArgumentException(stringBuilder.ToString());
+            }
+        }
+    }
+}
