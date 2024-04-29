29e6d6be7422a6797a70a1b32cc92826220d26b5
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-04-30 00:14:32 +0900

    テストプロジェクトからテスト対象のプロジェクトを無理やり参照するためのテストを追加した
    雑に入力もテストも元のDTOプロジェクトでやる

---
+++ MarkdownDocumentGenerator/MarkdownDocumentGenerator.Test/GetProjectPathSandboxTest.cs
@@ -0,0 +1,31 @@
+﻿namespace MarkdownDocumentGenerator.Test
+{
+    public class GetProjectPathSandboxTest
+    {
+        [Fact]
+        public void 相対パスでデバッグ用のプロジェクトを取得するテスト1()
+        {
+            var currentDirectory = new Uri(Environment.CurrentDirectory, UriKind.Absolute);
+            var relative = new Uri("../../../MarkdownDocumentGenerator/MarkdownDocumentGenerator.csproj", UriKind.Relative);
+
+            var projectPath = new Uri(currentDirectory, relative);
+
+            var existsProject = File.Exists(projectPath.AbsolutePath);
+
+            Assert.True(existsProject);
+        }
+
+        [Fact]
+        public void 相対パスでデバッグ用のプロジェクトを取得するテスト2()
+        {
+            var currentDirectory = new Uri(Environment.CurrentDirectory, UriKind.Absolute);
+            var relative = new Uri("../../../DTO/DTO.csproj", UriKind.Relative);
+
+            var projectPath = new Uri(currentDirectory, relative);
+
+            var existsProject = File.Exists(projectPath.AbsolutePath);
+
+            Assert.True(existsProject);
+        }
+    }
+}
