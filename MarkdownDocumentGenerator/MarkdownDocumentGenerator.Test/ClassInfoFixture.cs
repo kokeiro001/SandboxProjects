46a29310e012ab305f397fc45d11b4ca5ce9d7f9
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2024-05-04 10:45:34 +0900

    ClassInfoFixtureを新規ファイルに定義した

---
+++ MarkdownDocumentGenerator/MarkdownDocumentGenerator.Test/ClassInfoFixture.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace MarkdownDocumentGenerator.Test
+{
+    public class ClassInfoFixture
+    {
+        public ClassInfo[] ClassInfos { get; }
+
+        public ClassInfoFixture()
+        {
+            var currentDirectory = new Uri(Environment.CurrentDirectory, UriKind.Absolute);
+            var relative = new Uri("../../../DTO/DTO.csproj", UriKind.Relative);
+            var projectPath = new Uri(currentDirectory, relative);
+
+            MSBuildLocator.RegisterDefaults();
+            using var workspace = MSBuildWorkspace.Create();
+
+            var project = workspace.OpenProjectAsync(projectPath.AbsolutePath).Result;
+
+            var classInfoCollector = new ClassInfoCollector(project);
+
+            ClassInfos = classInfoCollector.Collect("DTO.DTOBase").Result;
+        }
+    }
+}
